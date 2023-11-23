78c719cf305f76bdcbd967c125f38775ab807e32
olesi <olesia.zamar@gmail.com>
2023-11-24 00:57:28 +0600

    настройка для https://www.appveyor.com/

--- src/__tests__/basic.js
+++ src/__tests__/basic.js
@@ -1,42 +1,42 @@
-//const { calculateTotal } = require('../calculate') //извлекаем пакет, синтаксис CommonJs
-import { calculateTotal } from '../calculate'
+// const { calculateTotal } = require('../calculate') //извлекаем пакет, синтаксис CommonJs
+import { calculateTotal } from '../calculate';
 
 test('calculateTotal sum', () => {
-    const list = [
-        {
-            id: 456,
-            name: 'Война и Мир',
-            count: 3,
-            price: 400
-        },
-        {
-            id: 77,
-            name: 'JavaScript',
-            count: 1,
-            price: 1200
-        }
-    ]
+  const list = [
+    {
+      id: 456,
+      name: 'Война и Мир',
+      count: 3,
+      price: 400,
+    },
+    {
+      id: 77,
+      name: 'JavaScript',
+      count: 1,
+      price: 1200,
+    },
+  ];
 
-    const result = calculateTotal(list)
-    expect(result).toBe(2400)
-})
+  const result = calculateTotal(list);
+  expect(result).toBe(2400);
+});
 
 test('calculateTotal sum without discount', () => {
-    const list = [
-        {
-            id: 456,
-            name: 'Война и Мир',
-            count: 3,
-            price: 401
-        },
-        {
-            id: 77,
-            name: 'JavaScript',
-            count: 1,
-            price: 1300
-        }
-    ]
+  const list = [
+    {
+      id: 456,
+      name: 'Война и Мир',
+      count: 3,
+      price: 401,
+    },
+    {
+      id: 77,
+      name: 'JavaScript',
+      count: 1,
+      price: 1300,
+    },
+  ];
 
-    const result = calculateTotal(list, true)
-    expect(result).toBeCloseTo(2230.173)
-})
+  const result = calculateTotal(list, true);
+  expect(result).toBeCloseTo(2230.173);
+});
--- src/__tests__/user.js
+++ src/__tests__/user.js
@@ -1,12 +1,13 @@
-import loadUser from "../loadUser";
-import {httpGet} from "../http";
-jest.mock('../http') //говорим, что именно этот файл должен стать моком
+import loadUser from '../loadUser';
+import { httpGet } from '../http';
 
-beforeEach(() => { //выполняем каждый раз, когда запускается тест
-    jest.resetAllMocks() //сбрасываем все моки
+jest.mock('../http'); // говорим, что именно этот файл должен стать моком
+
+beforeEach(() => { // выполняем каждый раз, когда запускается тест
+  jest.resetAllMocks(); // сбрасываем все моки
 });
-test('should call loadUser once', () => { //должен вернуть в строковом эквиваленте объект
-    httpGet.mockReturnValue(JSON.stringify({}));
-    loadUser(1);
-    expect(httpGet).toBeCalledWith('http://server:8080/users/1')
-})
+test('should call loadUser once', () => { // должен вернуть в строковом эквиваленте объект
+  httpGet.mockReturnValue(JSON.stringify({}));
+  loadUser(1);
+  expect(httpGet).toBeCalledWith('http://server:8080/users/1');
+});
